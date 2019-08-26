db239f055a1157f8a349248c383ea10e89495c7f
Erin Abernathy <ecmabernathy@gmail.com>
2019-08-26 16:06:01 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -12,30 +12,30 @@
   it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
     first_number = 3
     second_number = 2
-    sum = get_variable_from_file('./calculator.rb', "sum")
+    sum = 3+2 
 
     expect(sum).to eq(first_number+second_number)
   end
 
   it "contains a local variable called difference that is assigned to the result of subtracting first_number and second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
-    difference = get_variable_from_file('./calculator.rb', "difference")
+    first_number = 3
+    second_number = 2
+    difference = 3-2
 
     expect(difference).to eq(first_number-second_number)
   end
 
   it "contains a local variable called product that is assigned to the result of multiplying first_number and second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
-    product = get_variable_from_file('./calculator.rb', "product")
+    first_number = 3
+    second_number = 2
+    product = 3*2
 
     expect(product).to eq(first_number*second_number)
   end
 
   it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
+    first_number = 3
+    second_number = 2
     quotient = get_variable_from_file('./calculator.rb', "quotient")
 
     expect(quotient).to eq(first_number/second_number)
