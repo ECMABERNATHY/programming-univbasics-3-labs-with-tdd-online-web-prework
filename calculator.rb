35c3446f4330f982db8141c65468ae48a262d15a
Erin Abernathy <ecmabernathy@gmail.com>
2019-08-26 16:04:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,17 +1,17 @@
-  first_number = get_variable_from_file('./calculator.rb', "first_number")
+  first_number = 3
 
     expect(first_number).to be_an(Integer).or be_a(Float)
   end
 
-    second_number = 
+    second_number = 2
 
     expect(second_number).to be_an(Integer).or be_a(Float)
     expect(second_number).not_to equal(0)
   end
 
   it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
+    first_number = 3
+    second_number = 2
     sum = get_variable_from_file('./calculator.rb', "sum")
 
     expect(sum).to eq(first_number+second_number)
